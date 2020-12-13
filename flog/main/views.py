8f60865272e5fb0bd62ff91efadbd57e386fa776
andyzhou <andyforever0108@outlook.com>
2020-12-13 16:21:14 +0800

    Add image management to sidebar

--- flog/main/views.py
+++ flog/main/views.py
@@ -96,7 +96,7 @@ def upload():
 
 @main_bp.route('/images/manage/')
 @login_required
-def manage_self_images():
+def manage_images():
     page = request.args.get('page', 1, int)
     if not current_user.is_administrator():
         pagination = current_user.images.order_by(Image.timestamp.desc()).paginate(
