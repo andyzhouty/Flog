110a501736c0b40b5fd147fd4e6c0e3d5ac2d47e
andyzhou <zhoutianyou11@gmail.com>
2021-06-19 21:40:30 +0800

    still use truncate provided by flask instead of jquery

--- flog/static/js/script.js
+++ flog/static/js/script.js
@@ -80,9 +80,4 @@ $(function () {
         $('#image-large').hide();
     });
     $('table').addClass('table table-bordered');
-    $('.post-content').ellipsis({
-        lines: 2,
-        ellipClass: 'ellip',
-        responsive: true
-    });
 });
