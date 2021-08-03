58d2da283d78d27163c5ea633a09149c49a7ef86
andyzhou <andyforever0108@outlook.com>
2021-08-03 13:55:21 +0800

    Set proper language for notification

--- flog/notifications.py
+++ flog/notifications.py
@@ -2,11 +2,31 @@
 MIT License
 Copyright (c) 2020 Andy Zhou
 """
-from flask_babel import _
+from functools import wraps
+from flask_babel import _, force_locale
 from flask import url_for
 from .models import db, Notification
 
 
+def with_receiver_locale(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if len(args) > 1:
+            receiver = args[1]
+        else:
+            receiver = kwargs["receiver"]
+        locale = ""
+        if receiver is not None and receiver.locale is not None:
+            locale = receiver.locale
+        else:
+            locale = "en_US"
+        with force_locale(locale):
+            return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@with_receiver_locale
 def push_follow_notification(follower, receiver):
     """
     Push a notification when someone is followed by another user.
@@ -21,6 +41,7 @@ def push_follow_notification(follower, receiver):
     db.session.commit()
 
 
+@with_receiver_locale
 def push_comment_notification(comment, receiver, page=1):
     """
     Push a notification when a post has a new comment or
@@ -35,7 +56,8 @@ def push_comment_notification(comment, receiver, page=1):
     db.session.commit()
 
 
-def push_collect_notification(collector, post, receiver):
+@with_receiver_locale
+def push_collect_notification(collector, receiver, post):
     """Push a notification when a post is collected."""
     message = _(
         """User <a href="%(profile_url)s">%(username)s</a> collected your <a href="%(post_url)s">post</a>""",
@@ -48,7 +70,8 @@ def push_collect_notification(collector, post, receiver):
     db.session.commit()
 
 
-def push_group_join_notification(joiner, group, receiver):
+@with_receiver_locale
+def push_group_join_notification(joiner, receiver, group):
     """Push a notification to the manager of a group when another user wants to join it."""
     message = _(
         """User <a href="%(profile_url)s">%(username)s</a> wants to join your group %(name)s.
@@ -63,7 +86,8 @@ def push_group_join_notification(joiner, group, receiver):
     db.session.commit()
 
 
-def push_group_invite_notification(inviter, group, receiver):
+@with_receiver_locale
+def push_group_invite_notification(inviter, receiver, group):
     """Push a notification to the invited user."""
     message = _(
         """User <a href="%(profile_url)s">%(username)s</a> invited you to group %(name)s.
@@ -78,6 +102,7 @@ def push_group_invite_notification(inviter, group, receiver):
     db.session.commit()
 
 
+@with_receiver_locale
 def push_new_message_notification(sender, receiver, group):
     message = _(
         """<a href="%(profile_url)s">%(username)s</a> send a message in <a href="%(info_url)s">Group %(name)s</a>""",
--- tests/test_language.py
+++ tests/test_language.py
@@ -2,9 +2,8 @@
 MIT License
 Copyright (c) 2020 Andy Zhou
 """
-import os
-from .helpers import login
-from flog.models import User, Role
+from .helpers import generate_post, login, logout, register
+from flog.models import db, User, Role, Post
 
 
 def test_language_selection(client):
@@ -21,3 +20,28 @@ def test_no_login_language_selection(client):
     client.get("/language/set-locale/zh_Hans_CN/", follow_redirects=True)
     data = client.get("/").get_data(as_text=True)
     assert "加入我们" in data
+
+
+def test_notification_language(client):
+    """Test the notification received is in the receiver's language instead of the sender's."""
+    # generate a post 
+    login(client)
+    admin = User.query.filter_by(
+        role=Role.query.filter_by(name="Administrator").first()
+    ).first()
+    admin.locale = "zh_Hans_CN"
+    db.session.commit()
+    title = generate_post(client)["title"]
+    post = Post.query.filter_by(title=title).first()
+    logout(client)
+
+    register(client)
+    login(client, "test", "password")
+    user = User.query.filter_by(username="test").first()
+    user.locale = "en_US"
+    db.session.commit()
+    # test using the post collecting route
+    client.get(f"/post/collect/{post.id}/")
+    logout(client)
+
+    assert "文章" in admin.notifications[-1].message
